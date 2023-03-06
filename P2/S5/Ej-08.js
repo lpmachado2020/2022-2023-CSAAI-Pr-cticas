f43d9c7c028ad2fcd72e37d199992586e8a2c06a
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2023-03-07 00:19:56 +0100

    Fin teoria sesion 5

--- P2/S5/Ej-08.js
+++ P2/S5/Ej-08.js
@@ -1,7 +1,9 @@
+// Este no se por que no funciona
+
 console.log("Ejecutando JS...");
 
 const botones = document.getElementsByClassName("digito");
 
 for (let boton of botones) {
-    console.log("Boton: " +  boton.value)
+    console.log("Boton: " +  boton.value);
 }
