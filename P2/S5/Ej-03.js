d54b870178a856f94803a3ac4a73b4c655935b52
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2023-03-06 14:45:09 +0100

    Ejemplo 3 S5

---
+++ P2/S5/Ej-03.js
@@ -0,0 +1,25 @@
+console.log("Ejecutando JS...");
+
+//-- Crear objeto gui, con los elementos de la interfaz gráfica. Lo que hay en el body
+//-- Al tenerlo agrupado podemos pasarlo como parámetro o asignarlo
+//-- a otro objeto
+const gui = {
+  display: document.getElementById("display"),
+  boton: document.getElementById("boton"),
+}
+
+//-- Objeto contador: Contiene el valor y el método para incrementarse
+//-- inc es el metodo para incrementar el valor 0
+const counter = {
+  valor: 0,
+  inc : function(value) {
+    this.valor += value;
+    gui.display.innerHTML = this.valor;
+  }
+}
+
+//-- Acciones: Ligar el botón al contador
+//-- El counter.inc(1) indica que queremos incrementar 1 el valor 0 mediante el metodo inc
+gui.boton.onclick = () => {
+  counter.inc(1)
+}
