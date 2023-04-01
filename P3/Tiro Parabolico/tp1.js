88eae24c636e54b08795759fdbe2cf857842a15c
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2023-04-01 22:44:19 +0200

    mas cambios tp

--- P3/Tiro Parabolico/tp1.js
+++ P3/Tiro Parabolico/tp1.js
@@ -1,23 +1,71 @@
+console.log("Ejecutando JS...");
+
 //-- Declaración de variables y objetos
+
 
 //-- Obtención del canvas y de los elementos HTML a usar
 
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+const canvas = document.getElementById("canvas");
+
 //-- Función principal de actualización
 function update() 
+{
+  
+  requestAnimationFrame(update);
+}
+
+//-- Otras funciones....
+
+//-- función para pintar el proyectil
+function dibujarP(x,y,lx,ly,color) {
+
+  //-- Pintando el proyectil
+  ctx.beginPath();
+
+  //-- Definir un rectángulo de dimensiones lx x ly,
+  ctx.rect(x, y, lx, ly);
+
+  //-- Color de relleno del rectángulo
+  ctx.fillStyle = color;
+
+  //-- Mostrar el relleno
+  ctx.fill();
+
+  //-- Mostrar el trazo del rectángulo
+  ctx.stroke();
+
+  ctx.closePath();
+}
+
+//-- Coordenadas iniciales del proyectil
+let xop = 5;
+let yop = 345;
+let xp = xop;
+let yp = yop;
+
+//-- Dibujar el proyectil
+dibujarP(xop, yop, 50, 50, "green"); // Pintar el proyectil
+
+
+//-- Función principal de actualización
+function lanzar() 
 {
   //-- Implementación del algoritmo de animación:
 
   //-- 1) Actualizar posición de los elementos
 
   //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //-- 3) Pintar los elementos en el canvas
+  dibujarP(xp, yp, 50, 50, "blue"); // Pintar el proyectil
 
   //-- 4) Repetir
-  requestAnimationFrame(update);
+  requestAnimationFrame(lanzar);
 }
-
-//-- Otras funciones....
 
 //-- Hay que llamar a update la primera vez
 update();
