acf802b3cade5efec2979b4b60b826efe86f5fcc
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2023-04-13 00:48:18 +0200

    mas avanes

--- P4/Memory/memory-game.js
+++ P4/Memory/memory-game.js
@@ -1,27 +1,62 @@
 console.log("Ejecutando JS...");
 
-const movimientos = document.getElementsByClassName("movimientos");
-const tablero = document.getElementsByClassName("tablero");
-const card_front = document.getElementsByClassName("card-front");
-const card_back = document.getElementsByClassName("card-back");
+const selectors = {
+    gridContainer: document.querySelector('.grid-container'),
+    tablero: document.querySelector('.tablero'),
+    movimientos: document.querySelector('.movimientos'),
+    timer: document.querySelector('.timer'),
+    comenzar: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
 
-//-- Elementos de la gui
-const gui = {
-    timer: document.getElementsByClassName("timer")
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
 }
-  
-//-- Definir un objeto cronómetro
-const crono = new Crono(gui.timer);
-  
-//-- Función de retrollamada del botón de disparo
-gui.onclick = () => {
-    crono.start();
-    console.log("Start!!!")
+
+const generateGame = () => {
+    const dimensions = selectors.tablero.getAttribute('grid-dimension')
+
+    //-- Nos aseguramos de que el número de dimensiones es par
+    // y si es impar lanzamos un error
+    if (dimensions % 2 !== 0) {
+        throw new Error("Las dimensiones del tablero deben ser un número par.")
+    }
+
+    //-- Creamos un array con los emojis que vamos a utilizar en nuestro juego
+    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
+    
+    //-- Elegimos un subconjunto de emojis al azar, así cada vez que comienza el juego
+    // es diferente.
+    // Es decir, si tenemos un array con 10 emojis, vamos a elegir el cuadrado de las
+    // dimensiones entre dos, para asegurarnos de que cubrimos todas las cartas
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
+
+    //-- Después descolocamos las posiciones para asegurarnos de que las parejas de cartas
+    // están desordenadas.
+    const items = shuffle([...picks, ...picks])
+    
+    //-- Vamos a utilizar una función de mapeo para generar 
+    //  todas las cartas en función de las dimensiones
+    const cards = `
+        <div class="tablero" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `
+    
+    //-- Vamos a utilizar un parser para transformar la cadena que hemos generado
+    // en código html.
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    //-- Por último, vamos a inyectar el código html que hemos generado dentro de el contenedor
+    // para el tablero de juego.
+    selectors.tablero.replaceWith(parser.querySelector('.tablero'))
 }
-  
-//-- Función de retrollamada del botón iniciar
-gui.onclick = () => {
-    location.reload();
-    console.log("Reset!!!")
-}
-  
